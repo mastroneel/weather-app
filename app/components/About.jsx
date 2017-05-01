a5f91946a25b48cdfedbf956dd6f8d75d80462e6
Lauren Mastroni <laurenmastroni@gmail.com>
2017-05-01 18:27:41 +0800

    Add styles to Weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application built on React.
+      </p>
     </div>
   )
 };
