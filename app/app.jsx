a2116d0a61b3b104393ac484b4927275b2036cef
Lauren Mastroni <laurenmastroni@gmail.com>
2017-05-02 13:47:05 +0800

    Add app.css

--- app/app.jsx
+++ app/app.jsx
@@ -12,6 +12,9 @@ var WeatherMessage = require('WeatherMessage');
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
+// App css
+require('style!css!applicationStyles')
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
